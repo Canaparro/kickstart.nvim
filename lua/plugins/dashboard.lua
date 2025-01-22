53d696cf1c25c08c65957fdf4be81019c2d02dd9
canaparro <marcelocanaparro@gmail.com>
2025-01-21 20:07:57 -0800

    Adds dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -5,6 +5,29 @@ return {
     require('dashboard').setup {
       -- config
       config = {
+        shortcut = {
+          { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+          {
+            icon = ' ',
+            icon_hl = '@variable',
+            desc = 'Files',
+            group = 'Label',
+            action = 'Telescope find_files',
+            key = 'f',
+          },
+          {
+            desc = ' Apps',
+            group = 'DiagnosticHint',
+            action = 'Telescope app',
+            key = 'a',
+          },
+          {
+            desc = ' dotfiles',
+            group = 'Number',
+            action = 'Telescope dotfiles',
+            key = 'd',
+          },
+        },
         week_header = {
           enable = true,
         },
